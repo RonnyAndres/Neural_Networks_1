145c93cea51d774af69c59a39cb182ed83c36b54
RonnyAndres <76404004+RonnyAndres@users.noreply.github.com>
2023-07-13 18:25:03 +0000

    Mejoras

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-button = undefined
-const dropArea = document.querySelector(".drop_box"),
-  button = dropArea.querySelector("button"),
-  dragText = dropArea.querySelector("header"),
-  input = dropArea.querySelector("input");
+const dropArea2 = document.querySelector(".drop_box");
+const button = dropArea2.querySelector(".btn");
+const dragText = dropArea2.querySelector("header");
+const input = dropArea2.querySelector("input");
+
 let file;
 var filename;
 
 button.onclick = () => {
-  input.click();
-  console.log("button clicked");
+    input.click();
+    console.log("button clicked");
 };
 
 input.addEventListener("change", function (e) {
-  var fileName = e.target.files[0].name;
-  let filedata = `
-    <form action="" method="post">
-    <div class="form">
-    <h4>${fileName}</h4>
-    <button class="btn">Upload</button>
-    </div>
-    </form>`;
-  dropArea.innerHTML = filedata;
+    var file = e.target.files[0];
+    var fileName = file.name;
+    console.log(file.size);
+
+
+    
+    if (isImage(fileName)) {
+        // Evalar el tamaño de los pixeles de la imagen, tieen que ser de 28x28 pixeles
+
+        let fileReader = new FileReader();
+        fileReader.onload = function () {
+            var filedata = `
+                <form action="" method="post">
+                <div class="form" >
+                <h4>${fileName}</h4>
+                <img src="${fileReader.result}" alt="Uploaded Image" />
+                <button class="btn">Process</button>
+                </div>
+                </form>`;   
+            dropArea2.innerHTML = filedata;
+
+        };
+        
+        
+
+        fileReader.readAsDataURL(file);
+    }else{
+        alert("Please upload an image of 28x28 pixels");
+    }
+        
 });
 
-// Create a function that evauluate if the file is jpg, jpeg o png
+// Create a function that evaluates if the file is jpg, jpeg or png
 function isImage(filename) {
     var ext = filename.split(".").pop();
     switch (ext) {
         case "jpg":
         case "jpeg":
         case "png":
-        //etc
-        return true;
+            return true;
     }
     return false;
-    }
+}
